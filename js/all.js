bcef98c8fb5fcf3b0f3eab49e4c6a8eea96b7a4c
chris1085 <itscfchiang@gmail.com>
2021-06-15 01:59:40 +0800

    feat: 加入header, footer darkMode

--- js/all.js
+++ js/all.js
@@ -5,6 +5,8 @@ const darkModeToggle = document.getElementById('darkModeToggle')
 const darkModeToggleHandler = (event) => {
   document.documentElement.classList.toggle('lightTheme')
   document.documentElement.classList.toggle('darkTheme')
+  darkModeToggle.childNodes[0].classList.toggle('fa-sun')
+  darkModeToggle.childNodes[0].classList.toggle('fa-moon')
 }
 // bind the event
 darkModeToggle.addEventListener('click', darkModeToggleHandler)
